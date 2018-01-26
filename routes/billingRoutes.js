c03ec0c12e5183ee78507f39d848cf5e624c4f86
wade boam <wade@heckledesign.com>
2018-01-26 11:56:05 -0700

    Publishable key work

--- routes/billingRoutes.js
+++ routes/billingRoutes.js
@@ -1,6 +1,8 @@
 const keys = require('../config/keys');
 //var stripe = require('stripe')(keys.stripeSecretKey);
-var stripe = require('stripe')(keys.stripePublishableKey);
+//var stripe = require('stripe')(keys.stripePublishableKey);
+
+const stripe = require('stripe')('pk_test_6pRNASCoBOKtIshFeQd4XMUh');
 const requireLogin = require('../middlewares/requireLogin');
 
 module.exports = app => {
@@ -14,6 +16,7 @@ module.exports = app => {
             source: req.body.id
         });
         
+        console.log(charge);
         req.user.credits += 5;        
         const user = await req.user.save();        
         res.send(user);
