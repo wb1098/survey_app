30e990c5b36f024c91f7a6ae56e74dcf500c6af1
wade boam <wade@heckledesign.com>
2018-01-26 09:58:49 -0700

    Quick space update

--- server.js
+++ server.js
@@ -25,16 +25,13 @@ require('./public/routes/authRoutes')(app);
 require('./public/routes/billingRoutes')(app);
 
 if(process.env.NODE_ENV === 'production'){
-    app.use(express.static('client/build'));
-    
+    app.use(express.static('client/build'));    
     const path = require('path');
-    app.get('*', (req,res) => {
-        
+    app.get('*', (req,res) => {        
        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')); 
     });
     
 }
-
 
 
 var currentPort;
